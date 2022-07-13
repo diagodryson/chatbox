5623195d11b3a1a10ebd9f37f7ba02e548c04368
Diago Dryson <cortezdiago@gmail.com>
2022-07-13 11:18:10 +0200

    update

--- www/connexion.php
+++ www/connexion.php
@@ -1,11 +1,25 @@
+<!-- espace ADMIN -->
 <?php
 /* lorsque l'utiliser cliquer sur le code, il exécute le code */
 if(isset($_POST['valider'])){
+    /* test */
+    $pseudo_par_defaut = "admin";
+    $mdp_par_defaut = "admin1234";
+/* il stocke le pseudo saisi par l'utilisateur*/
+    $pseudo_saisi = htmlspecialchars($_POST['pseudo']);
+    $mdp_saisi = htmlspecialchars($_POST['mdp']);
+
+    /* vérifier si par défaut corresponde bien au saisi */
+    if($pseudo_saisi == $pseudo_par_defaut AND $mdp_saisi == $mdp_par_defaut){
+
+    }else{
+        echo "votre mot de passe ou pseudo est incorrect..";
+    }
     /* si ces deux champs ne sont pas vide on pourra exécuter notre code */
     if(!empty($_POST['pseudo']) AND !empty($_POST['mdp'])){
 
     }else{
-        echo "T'as oublié de compléter tous les champs..";
+        echo "Vous avez oublié de compléter tous les champs..";
     }
 }
 ?>
