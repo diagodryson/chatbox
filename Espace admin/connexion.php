3d5a75cd7d278bac5339afcf4d0b3c9028ddae11
Diago Dryson <cortezdiago@gmail.com>
2022-07-13 09:42:51 +0200

    Update connexion.php

--- Espace admin/connexion.php
+++ Espace admin/connexion.php
@@ -1,10 +1,22 @@
+<?php
+/* lorsque l'utiliser cliquer sur le code, il exécute le code */
+if(isset($_POST['valider'])){
+    /* si ces deux champs ne sont pas vide on pourra exécuter notre code */
+    if(!empty($_POST['pseudo']) AND !empty($_POST['mdp'])){
+
+    }else{
+        echo "T'as oublié de compléter tous les champs.."
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
         <title> Espace de connexion admin</title>
         <meta charset="utf-8">
 </head>
-   <form>
+<!-- envoyer des données au serveur -->
+   <form method="POST" action="" align="center">
     <input type="text" name="pseudo" autocomplete="off">
     <br>
     <input type="password" name="mdp">
