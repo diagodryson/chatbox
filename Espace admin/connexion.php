9c3f5def23431317313ea08b105881463ec448ae
Diago Dryson <cortezdiago@gmail.com>
2022-07-13 09:27:25 +0200

    Create connexion.php

---
+++ Espace admin/connexion.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title> Espace de connexion admin</title>
+        <meta charset="utf-8">
+</head>
+   <form>
+    <input type="text" name="pseudo" autocomplete="off">
+    <br>
+    <input type="password" name="mdp">
+    <br><br>
+    <input type="submit" name="valider">
+   </form>
+
+</html>
